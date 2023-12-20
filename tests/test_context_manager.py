806284e10fd2d15cf6e1a14852dd91e63145e936
Surya Kant Sahu <surya.oju@gmail.com>
2023-12-21 01:12:15 +0530

    make use_manipulators arg mandatory for avoid misuse

--- tests/test_context_manager.py
+++ tests/test_context_manager.py
@@ -54,26 +54,26 @@ def test_contextmanager():
     message = "<agent>Hello! Am I speaking with [first_name] [last_name]?"
 
     cm.stitch_bot_message_to_context(message)
-    assert cm.get_context() == result1
+    assert cm.get_context(use_manipulators=True) == result1
 
     # user generates the following:
     message = "<user>Okay"
 
     cm.stitch_user_message_to_context(message)
-    assert cm.get_context() == result2
+    assert cm.get_context(use_manipulators=True) == result2
 
     # agent generates the following:
     message = '''<agent>Alright, I am sending the message right away.
 <API>SMSPaymentLink(amount="$[due_amount]")'''
 
     cm.stitch_bot_message_to_context(message)
-    assert cm.get_context() == result3
+    assert cm.get_context(use_manipulators=True) == result3
 
     # system generates the following:
     message = '''<system>{status:success}'''
 
     cm.stitch_system_message_to_context(message)
-    assert cm.get_context() == result4
+    assert cm.get_context(use_manipulators=True) == result4
 
 
 context5 = '''USER_METADATA: {'first_name': 'Sarah', 'last_name': 'Brown', 'due_amount': 1562, 'last_4_digits_phone_number': 2457, 'user_address': '456 Oakwood Avenue Austin Texas', 'vehicle_details': '2016 Honda Accord'}
@@ -171,19 +171,19 @@ def test_contextmanager_unknown_turns():
     cm = ContextManager("")
     cm.context = context5
 
-    assert cm.get_context() == result5
+    assert cm.get_context(use_manipulators=True) == result5
 
     cm = ContextManager("")
     cm.context = context6
 
-    assert cm.get_context() == result6
+    assert cm.get_context(use_manipulators=True) == result6
 
     cm = ContextManager("")
     cm.context = context7
 
-    assert cm.get_context() == result7
+    assert cm.get_context(use_manipulators=True) == result7
 
     cm = ContextManager("")
     cm.context = context8
 
-    assert cm.get_context() == result8
+    assert cm.get_context(use_manipulators=True) == result8
